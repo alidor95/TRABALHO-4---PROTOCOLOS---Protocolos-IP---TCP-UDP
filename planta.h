d7f2d1477209c4a9872e0b27c82227ce9acd73bc
Enzo Burille <enzo_burille@hotmail.com>
2026-06-22 20:33:08 -0300

    Ajuste setMax

--- planta.h
+++ planta.h
@@ -12,7 +12,7 @@
 
 #define ANGULO_ENTRADA_INICIO  50.0   // angulo inicial da valvula de entrada
 #define NIVEL_INICIO           0.4    //nivel inicial é a porcetagem de 40%
-#define MAXIMO_INICIO          50.0    // saida inicial do tanque
+#define MAXIMO_INICIO          100.0    // saida inicial do tanque
 #define TAXA_VALVULA           0.015  // taxa da valvula de entrada por millisegundo
 
 
